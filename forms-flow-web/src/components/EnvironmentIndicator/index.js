95906153776ec15cda30ef6cbade48a199344fb5
dinesh <dinesh.pb@aot-technologies.com>
2022-09-25 20:50:55 +0530

    Added a banner to show the current environment in which the app is running

---
+++ forms-flow-web/src/components/EnvironmentIndicator/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import EnvironmentIndicator from "./EnvironmentIndicator";
+
+export default React.memo(() => {
+    return <EnvironmentIndicator />;
+  });
+  
---
+++ forms-flow-web/src/constants/additionalConstants.js
@@ -0,0 +1,4 @@
+export const APP_ENV =
+  (window._env_ && window._env_.APP_ENV) ||
+  process.env.APP_ENV ||
+  "";
