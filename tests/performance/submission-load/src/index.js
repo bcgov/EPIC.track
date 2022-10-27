f7ca04b1cdffee7dbb18fb1442abd40a0aa11d96
Dinesh <97143739+dinesh-aot@users.noreply.github.com>
2022-10-27 18:23:30 +0530

    Update submission-load.yml

--- tests/performance/submission-load/src/index.js
+++ tests/performance/submission-load/src/index.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 import { check } from 'k6';
-import {submission_data} from './data/submission_data.js';
-import {application_data} from './data/application.js';
+import { submission_data } from './data/submission_data.js';
+import { application_data } from './data/application.js';
 // init code
 const KEYCLOAK_URL = __ENV.KEYCLOAK_URL;
 const KEYCLOAK_REALM = __ENV.KEYCLOAK_REALM || 'forms-flow-ai';
@@ -11,6 +11,7 @@ const PASSWORD = __ENV.KEYCLOAK_PASSWORD;
 const FORMID = __ENV.FORMID;
 const FORM_URL = __ENV.FORMIO_URL;
 const APPLICATION_API = __ENV.APPLICATION_API;
+const WEB_URL = __ENV.WEB_URL;
 const SUBMISSION_URL = `${FORM_URL}/form/${FORMID}/submission`;
 const APPLICATION_CREATE_URL = `${APPLICATION_API}/application/create`;
 const ROLES_URL = `${APPLICATION_API}/formio/roles`;
@@ -26,7 +27,7 @@ export function setup() {
         grant_type: 'password',
         client_id: KEYCLOAK_CLIENT_ID
     }
-    
+
     var params = {
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
@@ -34,17 +35,17 @@ export function setup() {
     }
     var token_response = http.post(`${KEYCLOAK_URL}/auth/realms/${KEYCLOAK_REALM}/protocol/openid-connect/token`, body, params);
     check(token_response, {
-        '(Access Token)reponse code was 200': (res)=>res.status === 200,
+        '(Access Token)reponse code was 200': (res) => res.status === 200,
         '(Access Token)response had access_token': (res) => JSON.parse(res.body).access_token !== undefined
     })
     var token = JSON.parse(token_response.body).access_token;
-    var roles_response = http.get(ROLES_URL,{
+    var roles_response = http.get(ROLES_URL, {
         headers: {
             'Authorization': `Bearer ${token}`
         }
     })
     console.log('ACCESS token', JSON.parse(token_response.body).access_token);
-    check(roles_response,{
+    check(roles_response, {
         '(Formio Token)should have formio token': (res) => res.headers['X-Jwt-Token'] !== undefined
     })
     var formio_token = roles_response.headers['X-Jwt-Token'];
@@ -54,27 +55,30 @@ export function setup() {
     }
 }
 export default function (result) {
- var params = {
-    headers: {
-        'Authorization': `Bearer ${result.token}`,
-        'Content-Type': 'application/json',
-        'X-Jwt-Token': result.formio_token
+    var params = {
+        headers: {
+            'Authorization': `Bearer ${result.token}`,
+            'Content-Type': 'application/json',
+            'X-Jwt-Token': result.formio_token
+        }
     }
- }
- var data = JSON.stringify(submission_data);
+    var data = JSON.stringify(submission_data);
 
- var submission_response = http.post(SUBMISSION_URL, data, params);
- check(submission_response, {
-    '(Submission)response code was 201': (res)=>res.status === 201,
-    '(Submission)should have submission id': (res)=>JSON.parse(res.body)._id !== undefined
- })
- var submission_id = JSON.parse(submission_response.body)._id;
-console.log('submission id ',submission_id);
- var app_data = JSON.stringify(application_data).replace(/#submissionid/g, submission_id)
- var application_create_response = http.post(APPLICATION_CREATE_URL, app_data, params);
- check(application_create_response, {
-    '(Application Create)respone code was 201': (res) => res.status === 201,
-    '(Application Create)application id should be created': (res) => JSON.parse(res.body).id !== undefined
- });
+    var submission_response = http.post(SUBMISSION_URL, data, params);
+    check(submission_response, {
+        '(Submission)response code was 201': (res) => res.status === 201,
+        '(Submission)should have submission id': (res) => JSON.parse(res.body)._id !== undefined
+    })
+    var submission_id = JSON.parse(submission_response.body)._id;
+    console.log('submission id ', submission_id);
+    application_data.formId = FORMID;
+    application_data.submissionId = submission_id;
+    application_data.formUrl = `${SUBMISSION_URL}/${submission_id}`;
+    application_data.webFormUrl= `${WEB_URL}/form/${FORMID}/submission/${submission_id}`;
+    var application_create_response = http.post(APPLICATION_CREATE_URL, application_data, params);
+    check(application_create_response, {
+        '(Application Create)respone code was 201': (res) => res.status === 201,
+        '(Application Create)application id should be created': (res) => JSON.parse(res.body).id !== undefined
+    });
 
 }
