3b1d60c013429817680d318dc1e8f272d1f7600d
Salabh A N <salabh.n@aot-technologies.com>
2022-12-07 13:07:36 +0530

    Amendments (#29)
    add amendments work types through migrations
     - simple amendment
     - typical amendment
     - complex amendment

--- reports-api/migrations/env.py
+++ reports-api/migrations/env.py
@@ -77,10 +77,13 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
+        print("=" * 60)
+        print(f"compare_type = {current_app.extensions['migrate'].configure_args}")
+        # print(f"compare_type = {compare_type}")
+        print("=" * 60)
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
-            compare_type=True,
             process_revision_directives=process_revision_directives,
             **current_app.extensions['migrate'].configure_args
         )
