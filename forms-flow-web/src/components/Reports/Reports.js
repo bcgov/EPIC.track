f935d8937b0243ad4302622fc5130b7588f3d4b3
dinesh <dinesh.pb@aot-technologies.com>
2023-02-07 22:31:34 -0800

    report module added

---
+++ forms-flow-web/src/components/Reports/Reports.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../epicflow-web/epicflow-web.0.1.0';
+import { WEB_BASE_CUSTOM_URL } from '../../constants/constants';
+export default function Reports() {
+    return (
+        <div>
+            <anticipated-eao-schedule-wc apiUrl={WEB_BASE_CUSTOM_URL}></anticipated-eao-schedule-wc>
+        </div>
+    );
+}
