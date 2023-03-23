63795becc6dbeb9f05a0b953bc15eaf93f341b02
Dinesh <97143739+dinesh-aot@users.noreply.github.com>
2023-03-22 19:45:37 -0700

    Showing all fields in resource forecast with horizontal scroll (#436)
    * showing all fields in the resource forecast

--- epicflow-web/src/components/reports/resourceForecast/resourceForecast.tsx
+++ epicflow-web/src/components/reports/resourceForecast/resourceForecast.tsx
@@ -1,11 +1,9 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import {
-  Alert, Box, Button, Container, FormControl, FormLabel, Grid, MenuItem, OutlinedInput,
-  Skeleton
+  Alert, Box, Button, Container, FormLabel, Grid
 } from '@mui/material';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import Select from '@mui/material/Select';
 import MaterialReactTable from 'material-react-table';
 import { RESULT_STATUS, REPORT_TYPE, DATE_FORMAT } from '../../../constants/application-constant';
 import ReportService from '../../../services/reportService';
@@ -15,30 +13,30 @@ export default function ResourceForecast() {
   const [reportDate, setReportDate] = useState<string>();
   const [resultStatus, setResultStatus] = useState<string>();
   const [rfData, setRFData] = useState<any[]>([]);
-  const [selectedFilters, setSelectedFilter] = useState<string[]>([]);
+  // const [selectedFilters, setSelectedFilter] = useState<string[]>([]);
 
-  const filters = [
-    {
-      label: 'Capital Investment',
-      value: 'capital_investment'
-    },
-    {
-      label: 'Responsible EPD',
-      value: 'responsible_epd'
-    },
-    {
-      label: 'EAO Team',
-      value: 'eao_team'
-    },
-    {
-      label: 'Work Team Members',
-      value: 'work_team_members'
-    },
-    {
-      label: 'Referral Timing',
-      value: 'referral_timing'
-    }];
-  const setDynamicColumns = useCallback(() => {
+  // const filters = [
+  //   {
+  //     label: 'Capital Investment',
+  //     value: 'capital_investment'
+  //   },
+  //   {
+  //     label: 'Responsible EPD',
+  //     value: 'responsible_epd'
+  //   },
+  //   {
+  //     label: 'EAO Team',
+  //     value: 'eao_team'
+  //   },
+  //   {
+  //     label: 'Work Team Members',
+  //     value: 'work_team_members'
+  //   },
+  //   {
+  //     label: 'Referral Timing',
+  //     value: 'referral_timing'
+  //   }];
+  const setMonthColumns = useCallback(() => {
     let columns = [];
     if (rfData && rfData.length > 0) {
       columns = rfData[0].months.map((rfMonth: any, index: number) => {
@@ -81,8 +79,11 @@ export default function ResourceForecast() {
   const envRegionFilter = filterFn('env_region');
   const nrsRegionFilter = filterFn('nrs_region');
   const workLeadFilter = filterFn('work_lead');
+  const epdFilter = filterFn('responsible_epd');
+  const teamFilter = filterFn('eao_team');
+  const cairtLeadFilter = filterFn('cairt_lead');
 
-  const newColumns = useMemo(
+  const columns = useMemo(
     () => [
       {
         accessorKey: 'project_name',
@@ -90,6 +91,10 @@ export default function ResourceForecast() {
         enableHiding: false,
         filterVariant: 'select',
         filterSelectOptions: projectFilter
+      },
+      {
+        accessorKey: 'capital_investment',
+        header: 'Estimated Capital Investment'
       },
       {
         accessorKey: 'ea_type',
@@ -117,8 +122,7 @@ export default function ResourceForecast() {
       },
       {
         accessorFn: (row: any): any => `${row.type}( ${row.sub_type} )`,
-        accessorKey: 'sector',
-        header: 'Sector',
+        header: 'Project Type (Subtype)',
         filterVariant: 'select',
         filterSelectOptions: typeFilter
       },
@@ -135,37 +139,59 @@ export default function ResourceForecast() {
         filterSelectOptions: nrsRegionFilter
       },
       {
+        accessorKey: 'responsible_epd',
+        header: 'Responsible EPD',
+        filterVariant: 'select',
+        filterSelectOptions: epdFilter
+      },
+      {
+        accessorKey: 'cairt_lead',
+        header: 'FN CAIRT Lead',
+        filterVariant: 'select',
+        filterSelectOptions: cairtLeadFilter
+      },
+      {
+        accessorKey: 'eao_team',
+        header: 'Lead\'s Team',
+        filterVariant: 'select',
+        filterSelectOptions: teamFilter
+      },
+      {
         accessorKey: 'work_lead',
-        header: 'EPD Lead',
+        header: 'Work Lead',
         filterVariant: 'select',
         filterSelectOptions: workLeadFilter
       },
-      ...setDynamicColumns()
-    ], [rfData, setDynamicColumns]
+      {
+        accessorKey: 'work_team_members',
+        header: 'Work Team Members'
+      },
+      ...setMonthColumns(),
+      {
+        accessorKey: 'referral_timing',
+        header: 'Referral Timing',
+        enableHiding: true
+      }
+    ], [setMonthColumns]
   );
 
-  console.log(setDynamicColumns());
-  const getSelectedFilterLabels = (selected: string[]) => {
-    const labels = selected.map(val => {
-      return filters.filter(p => p.value === val)[0].label
-    });
-    return labels;
-  }
+  // const getSelectedFilterLabels = (selected: string[]) => {
+  //   const labels = selected.map(val => {
+  //     return filters.filter(p => p.value === val)[0].label
+  //   });
+  //   return labels;
+  // }
   const fetchReportData = async () => {
     setResultStatus(RESULT_STATUS.LOADING);
     try {
       const reportData =
         await ReportService.fetchReportData(REPORT_TYPE.RESOURCE_FORECAST, {
-          report_date: reportDate,
-          filters: {
-            exclude: selectedFilters
-          }
+          report_date: reportDate
         });
       setResultStatus(RESULT_STATUS.LOADED);
       if (reportData.status === 200) {
         setRFData((reportData.data as never)['data']);
       }
-      console.log(rfData);
       if (reportData.status === 204) {
         setResultStatus(RESULT_STATUS.NO_RECORD);
       }
@@ -176,7 +202,7 @@ export default function ResourceForecast() {
   return (
     <>
       <Grid component="form" onSubmit={(e) => e.preventDefault()}
-        container spacing={2} sx={{ marginTop: '5px' }}>
+        container spacing={2} sx={{ mt: '5px', mb: '15px' }}>
         <Grid item sm={2}><FormLabel>Report Date</FormLabel></Grid>
         <Grid item sm={2}>
           <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -189,7 +215,7 @@ export default function ResourceForecast() {
               }} />
           </LocalizationProvider>
         </Grid>
-        <Grid item sm={2}><FormLabel>Filter</FormLabel></Grid>
+        {/* <Grid item sm={2}><FormLabel>Filter</FormLabel></Grid>
         <Grid item sm={2}>
           <FormControl sx={{ width: 300 }}>
             <Select
@@ -214,8 +240,8 @@ export default function ResourceForecast() {
                 )}
             </Select>
           </FormControl>
-        </Grid>
-        <Grid item sm={resultStatus === RESULT_STATUS.LOADED ? 3 : 4}>
+        </Grid> */}
+        <Grid item sm={resultStatus === RESULT_STATUS.LOADED ? 7 : 8}>
           <Button variant='contained'
             type='submit'
             onClick={fetchReportData}
@@ -227,40 +253,23 @@ export default function ResourceForecast() {
             <Button variant='contained'>Download</Button>}
         </Grid>
       </Grid>
-      {resultStatus === RESULT_STATUS.LOADED &&
-        rfData &&
-        rfData.length > 0 &&
-        <MaterialReactTable
-          initialState={{
-            density: 'compact'
-          }}
-          columns={newColumns}
-          enableDensityToggle={false}
-          enableStickyHeader={true}
-          onColumnVisibilityChange={(state: any) => {
-            console.log(state);
-          }}
-          data={rfData} />
-      }
-      {resultStatus === RESULT_STATUS.NO_RECORD &&
-        <Container>
-          <Alert severity="warning">
-            No Records Found
-          </Alert>
-        </Container>}
+      <MaterialReactTable
+        initialState={{
+          density: 'compact'
+        }}
+        columns={columns}
+        enableDensityToggle={false}
+        enableStickyHeader={true}
+        state={{
+          isLoading: resultStatus === RESULT_STATUS.LOADING
+        }}
+        data={rfData} />
       {resultStatus === RESULT_STATUS.ERROR &&
         <Container>
           <Alert severity="error">
             Error occured during processing. Please try again after some time.
           </Alert>
         </Container>}
-      {resultStatus === RESULT_STATUS.LOADING &&
-        <>
-          <Skeleton />
-          <Skeleton animation="wave" />
-          <Skeleton animation={false} />
-        </>
-      }
     </>
   );
 }
