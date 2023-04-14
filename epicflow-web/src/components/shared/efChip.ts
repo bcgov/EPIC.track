750c2d201c48c6650ce2b8a629a22e94498998a8
Dinesh <97143739+dinesh-aot@users.noreply.github.com>
2023-04-14 10:53:05 -0700

    Chip style changed for stale level information on Referral Report (#551)

---
+++ epicflow-web/src/components/shared/efChip.ts
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Chip } from '@mui/material';
+import { styled } from '@mui/styles';
+
+const EFChip = styled(Chip)({
+  backgroundColor: 'rgb(213, 4, 4,0.1)',
+  color: 'rgb(213, 4, 4)',
+  borderRadius: '4px',
+  fontSize: '12px'
+})
+
+export default EFChip;
