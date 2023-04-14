97fbe3326f7319c091187cff10b6ee9f2d9f498d
dinesh <dinesh.pb@aot-technologies.com>
2023-04-14 10:52:31 -0700

    Chip style changed for stale level information on Referral Report

--- epicflow-web/src/components/reports/eaReferral/anticipatedEAOSchedule.tsx
+++ epicflow-web/src/components/reports/eaReferral/anticipatedEAOSchedule.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container } from '@mui/system';
 import {
-  Accordion, AccordionDetails, AccordionSummary, Alert, Box, Button, Chip, FormLabel,
+  Accordion, AccordionDetails, AccordionSummary, Alert, Box, Button, FormLabel,
   Grid, Skeleton, Tab, Table, TableBody, TableCell, TableRow, Tabs, Typography
 } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
@@ -12,6 +12,7 @@ import ReportService from '../../../services/reportService';
 import { RESULT_STATUS, REPORT_TYPE, DATE_FORMAT }
   from '../../../constants/application-constant';
 import { dateUtils } from '../../../utils';
+import EFChip from '../../shared/efChip';
 import moment from 'moment';
 
 export default function AnticipatedEAOSchedule() {
@@ -70,7 +71,7 @@ export default function AnticipatedEAOSchedule() {
     const dateObj = moment(date);
     const diff = dateObj.diff(moment(),'days');
     console.log(date,diff);
-    return diff >=0 ? 'success': (diff == -1)? 'warning': 'error';
+    return diff >=0 ? 'rgb(46, 125, 50, 0.1)': (diff == -1)? 'rgb(237, 108, 2, 0.1)': 'rgba(213, 4, 4, 0.1)';
   }, [])
 
   interface TabPanelProps {
@@ -139,9 +140,12 @@ export default function AnticipatedEAOSchedule() {
                     return <Accordion key={itemIndex} elevation={0}>
                       <AccordionSummary expandIcon={<ArrowForwardIosSharpIcon />}>
                         <Typography>
-                          <Chip style={{marginRight: '0.5rem'}} label={<>
+                          <EFChip style={{
+                            marginRight: '0.5rem',
+                            backgroundColor: `${staleLevel(item['date_updated'])}`
+                          }} label={<>
                             <b>{item['date_updated']}</b>
-                          </>} color={staleLevel(item['date_updated'])} />
+                          </>} />
                           {item['project_name']}
                         </Typography>
                       </AccordionSummary>
