a0cf79dafd55d8604dff937eb5629805e20a2511
Salabh A N <salabh.n@aot-technologies.com>
2023-05-20 04:12:49 +0530

    #494 - remaning work types PT2 (#649)
    * #494 - remaning work types PT2
    
    * data json file

---
+++ reports-api/migrations/versions/0ce638918228_merge_45123f9bd196_39211261a25b.py
@@ -0,0 +1,24 @@
+"""merge 45123f9bd196 & 39211261a25b
+
+Revision ID: 0ce638918228
+Revises: 45123f9bd196, 39211261a25b
+Create Date: 2023-05-19 17:51:53.552307
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0ce638918228'
+down_revision = ('45123f9bd196', '39211261a25b')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
