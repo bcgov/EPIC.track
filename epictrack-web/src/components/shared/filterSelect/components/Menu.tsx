36f802fc6f013ed8fcb750fa6174f3afa6d47e39
salabh-an <salabh.n@aot-technologies.com>
2023-08-17 16:58:19 +0530

    824 - Workplan Page - In-grid filters

---
+++ epictrack-web/src/components/shared/filterSelect/components/Menu.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Box, Button } from "@mui/material";
+
+import { components, MenuProps } from "react-select";
+import { Palette } from "../../../../styles/theme";
+import { ETParagraph } from "../..";
+
+const Menu = (props: MenuProps) => {
+  const { filterProps } = props.selectProps;
+  return (
+    <>
+      <Box
+        sx={{
+          display: "inline-flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          borderRadius: "4px",
+          border: `1px solid ${Palette.neutral.accent.light}`,
+          background: `${Palette.white}`,
+          boxShadow: "0px 4px 8px 0px rgba(0, 0, 0, 0.10)",
+        }}
+      >
+        <components.Menu {...props}>
+          {props.options.length > 5 && (
+            <Box
+              sx={{
+                display: "flex",
+                padding: ".5rem 1rem",
+                alignItems: "center",
+                gap: "1rem",
+                justifyContent: "space-between",
+                background: Palette.neutral.bg.light,
+              }}
+            >
+              {filterProps?.selectedOptions && (
+                <ETParagraph color={Palette.black}>
+                  {filterProps?.selectedOptions.length} selected
+                </ETParagraph>
+              )}
+              <Button variant="text" onClick={filterProps?.clearFilters}>
+                Clear Filters
+              </Button>
+            </Box>
+          )}
+          <Box sx={{ maxHeight: "300px", overflowY: "auto" }}>
+            {props.children}
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              padding: "1rem",
+              alignItems: "flex-start",
+              gap: "1rem",
+            }}
+          >
+            <Button variant="outlined" onClick={filterProps?.onCancel}>
+              Cancel
+            </Button>
+            <Button variant="contained" onClick={filterProps?.applyFilters}>
+              Apply
+            </Button>
+          </Box>
+        </components.Menu>
+      </Box>
+    </>
+  );
+};
+
+export default Menu;
---
+++ epictrack-web/src/components/shared/filterSelect/components/MultiValueContainer.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { MultiValueProps } from "react-select";
+import { Box } from "@mui/material";
+import { Palette } from "../../../../styles/theme";
+import { ETCaption2 } from "../..";
+
+const MultiValue = (props: MultiValueProps) => {
+  const { filterProps } = props.selectProps;
+
+  return (
+    <>
+      {props.index === 0 &&
+        filterProps?.selectedOptions &&
+        filterProps?.selectedOptions?.length > 0 && (
+          <Box
+            sx={{
+              display: "flex",
+              height: "2.25rem",
+              // padding: "0px 12px",
+              alignItems: "center",
+              alignSelf: "stretch",
+              borderRadius: "4px",
+              background: Palette.primary.bg.light,
+            }}
+            key={props.index}
+          >
+            <ETCaption2 bold color={Palette.primary.accent.light}>
+              {filterProps.variant === "inline"
+                ? "Filtered"
+                : `Filtered(${(props.selectProps.value as []).length})`}
+            </ETCaption2>
+          </Box>
+        )}
+      {props.index === 0 && !filterProps?.selectedOptions && (
+        <ETCaption2 color={Palette.neutral.light}>
+          {props.selectProps.placeholder}
+        </ETCaption2>
+      )}
+    </>
+  );
+};
+
+export default MultiValue;
