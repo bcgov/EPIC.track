3db273e073226735ea0f3948a74fa9c3a63cc5c9
Tom Chapman <tchapman000@gmail.com>
2023-10-30 09:28:50 -0700

    excluded responsibility ids from task creation

--- epictrack-api/src/api/services/task.py
+++ epictrack-api/src/api/services/task.py
@@ -144,6 +144,7 @@ class TaskService:
     def _prepare_task_event_object(cls, data: dict) -> dict:
         """Prepare a task event object"""
         exclude = ["assignee_ids"]
+        exclude = ["responsibility_ids"]
         return {key: data[key] for key in data.keys() if key not in exclude}
 
     @classmethod
