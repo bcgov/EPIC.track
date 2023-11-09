904acbba4fe16b85200a5355130e1336396e6322
TomChapmanGov <144270913+TomChapmanGov@users.noreply.github.com>
2023-11-09 11:48:38 -0800

    Status api integration (#1205)
    * remove unneeded changes
    
    * commented out status notes
    
    ---------
    
    Co-authored-by: saravanpa-aot <saravankumar.pa@aot-technologies.com>
    Co-authored-by: Tom Chapman <tchapman000@gmail.com>

--- epictrack-web/src/components/workPlan/status/StatusContainer/StatusNotes.tsx
+++ epictrack-web/src/components/workPlan/status/StatusContainer/StatusNotes.tsx
@@ -8,49 +8,45 @@ import debounce from "lodash/debounce";
 
 const StatusNotes = () => {
   const { statuses, getWorkStatuses } = React.useContext(WorkplanContext);
-  const { workId } = React.useContext(StatusContext);
-  const [notes, setNotes] = React.useState("");
+  // const { workId } = React.useContext(StatusContext);
+  // const [notes, setNotes] = React.useState("");
 
-  React.useEffect(() => {
-    setNotes(statuses[0]?.notes || "");
-  }, [statuses]);
+  // React.useEffect(() => {
+  //   setNotes(statuses[0]?.notes || "");
+  // }, [statuses]);
 
-  const saveStatusNotes = React.useCallback(async (value: string) => {
-    const result = await statusService.update(
-      Number(workId),
-      Number(statuses[0]?.id),
-      { notes: value, description: statuses[0]?.description }
-    );
-    if (result.status === 200) {
-      getWorkStatuses();
-      showNotification("Notes saved successfully", {
-        type: "success",
-        duration: 1000,
-      });
-    }
-  }, []);
+  // const saveStatusNotes = React.useCallback(async (value: string) => {
+  //   const result = await statusService.update(
+  //     Number(workId),
+  //     Number(statuses[0]?.id),
+  //     { notes: value, description: statuses[0]?.description }
+  //   );
+  //   if (result.status === 200) {
+  //     getWorkStatuses();
+  //     showNotification("Notes saved successfully", {
+  //       type: "success",
+  //       duration: 1000,
+  //     });
+  //   }
+  // }, []);
 
-  const debounceSave = React.useMemo(() => {
-    return debounce(saveStatusNotes, 1000);
-  }, [saveStatusNotes]);
+  // const debounceSave = React.useMemo(() => {
+  //   return debounce(saveStatusNotes, 1000);
+  // }, [saveStatusNotes]);
 
-  const handleNotesChange = (value: string) => {
-    if (value !== notes) {
-      setNotes(value);
-      debounceSave(value);
-    }
-  };
+  // const handleNotesChange = (value: string) => {
+  //   if (value !== notes) {
+  //     setNotes(value);
+  //     debounceSave(value);
+  //   }
+  // };
 
   return (
-    <>
-      {statuses.length > 0 && (
-        <RichTextEditor
-          // TODO re work so notes save on a work level not a status level
-          // handleEditorStateChange={handleNotesChange}
-          initialRawEditorState={statuses[0]?.notes}
-        />
-      )}
-    </>
+    <RichTextEditor
+    // TODO re work so notes save on a work level not a status level
+    // handleEditorStateChange={handleNotesChange}
+    // initialRawEditorState={statuses[0]?.notes}
+    />
   );
 };
 
