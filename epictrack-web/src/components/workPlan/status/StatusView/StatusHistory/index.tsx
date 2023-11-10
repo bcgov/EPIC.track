97130f7bc10cab21a0c2c8efe520bab8221f0e23
Tom Chapman <tchapman000@gmail.com>
2023-11-10 13:14:42 -0800

    added display limits to status history

--- epictrack-web/src/components/workPlan/status/StatusView/StatusHistory/index.tsx
+++ epictrack-web/src/components/workPlan/status/StatusView/StatusHistory/index.tsx
@@ -1,14 +1,30 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { WorkplanContext } from "../../../WorkPlanContext";
 import { Status } from "../../../../../models/status";
-import { ETCaption1 } from "../../../../shared";
+import { ETCaption1, ETCaption2 } from "../../../../shared";
 import Timeline from "@mui/lab/Timeline";
 import HistoryItem from "./HistoryItem";
-import { If, Then } from "react-if";
+import { Else, If, Then } from "react-if";
+import { IconProps } from "../../../../icons/type";
+import Icons from "../../../../icons";
+import { Palette } from "../../../../../styles/theme";
+
+const ExpandIcon: React.FC<IconProps> = Icons["ExpandIcon"];
+const CollapseIcon: React.FC<IconProps> = Icons["CollapseIcon"];
+
+const HISTORY_DISPLAY_LIMIT = 3;
 
 const StatusHistory = () => {
   const { statuses } = React.useContext(WorkplanContext);
+  const [indexLimit, setIndexLimit] = React.useState<boolean>(true);
+
+  const displayLimit = (index: number) => {
+    if (indexLimit && index > HISTORY_DISPLAY_LIMIT) {
+      return false;
+    }
+    return true;
+  };
 
   return (
     <Box sx={{ width: "50%" }}>
@@ -16,13 +32,38 @@ const StatusHistory = () => {
         STATUS HISTORY
       </ETCaption1>
       <Timeline position="left" sx={{ overflowY: "scroll", flex: 1 }}>
-        {statuses.map((status: Status) => (
-          <If condition={status.is_approved && statuses[0].id !== status.id}>
+        {statuses.map((status: Status, index: number) => (
+          <If
+            condition={
+              status.is_approved &&
+              statuses[0].id !== status.id &&
+              displayLimit(index)
+            }
+          >
             <Then>
               <HistoryItem status={status} />
             </Then>
           </If>
         ))}
+        <Button
+          onClick={() => setIndexLimit(!indexLimit)}
+          sx={{
+            gap: "8px",
+            width: "150px",
+            marginLeft: 12,
+          }}
+        >
+          <If condition={indexLimit}>
+            <Then>
+              <ExpandIcon />
+              <ETCaption2 bold> Show More</ETCaption2>
+            </Then>
+            <Else>
+              <CollapseIcon />
+              <ETCaption2 bold> Show Less</ETCaption2>
+            </Else>
+          </If>
+        </Button>
       </Timeline>
     </Box>
   );
