8e0d72c2b85712733b02b99c73b55d1c16f112ad
Jad <jad.saad@aot-technologies.com>
2023-11-10 17:39:32 -0800

    remove mock

--- epictrack-web/src/components/workPlan/issues/IssueAccordion/IssueHistory.tsx
+++ epictrack-web/src/components/workPlan/issues/IssueAccordion/IssueHistory.tsx
@@ -39,16 +39,7 @@ const IssueHistory = ({ issue }: { issue: WorkIssue }) => {
               <ETPreviewText
                 color={isSuccess ? Palette.neutral.dark : Palette.neutral.main}
               >
-                <ReadMoreText>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </ReadMoreText>
+                <ReadMoreText>{update.description}</ReadMoreText>
               </ETPreviewText>
             </TimelineOppositeContent>
             <TimelineSeparator>
