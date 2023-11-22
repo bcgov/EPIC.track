62e97ae4249709ff6c229bfc4480ab3235121bc2
Dinesh <dinesh.pb@aot-technologies.com>
2023-11-21 19:51:42 -0800

    project migration merge

---
+++ epictrack-api/migrations/versions/6a490ab27bd8_merging_heads.py
@@ -0,0 +1,24 @@
+"""merging heads
+
+Revision ID: 6a490ab27bd8
+Revises: 06f9e87c2f4b, fef6a8d8a58f
+Create Date: 2023-11-21 19:28:08.814495
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6a490ab27bd8'
+down_revision = ('06f9e87c2f4b', 'fef6a8d8a58f')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
