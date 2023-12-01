fbcb1024cb56ce8ae1d38e3492097dd22874c145
Jad <jad.saad@aot-technologies.com>
2023-11-30 20:01:02 -0800

    Add no results found

--- epictrack-web/src/components/myWorkplans/CardList.tsx
+++ epictrack-web/src/components/myWorkplans/CardList.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Box } from "@mui/material";
+import NoResultsFound from "../NoResultsFound";
 
 const CardList = () => {
+  const cards = [];
+
+  if (cards.length === 0) {
+    return <NoResultsFound />;
+  }
   return <Box>This is a card list</Box>;
 };
 
