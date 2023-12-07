038c38589682431fab9b23286bc3db80e9901486
Tom Chapman <tchapman000@gmail.com>
2023-12-07 11:54:32 -0800

    lazy loading initial implementation

--- epictrack-web/src/components/myWorkplans/MyWorkplanContainer.tsx
+++ epictrack-web/src/components/myWorkplans/MyWorkplanContainer.tsx
@@ -1,32 +1,43 @@
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { ETPageContainer } from "../shared";
 import { MyWorkplansContext } from "./MyWorkPlanContext";
 import CardList from "./CardList";
 import { Grid } from "@mui/material";
 import Filters from "./Filters";
 import { AssigneeToggle } from "./Filters/AssigneeToggle";
+import { useOnScreen } from "../../hooks";
 
 const WorkPlanContainer = () => {
-  const {} = useContext(MyWorkplansContext);
+  const { loadMoreWorkplans, loadingWorkplans } =
+    useContext(MyWorkplansContext);
+  const ref = useRef<HTMLDivElement>(null);
+  const isVisible = useOnScreen(ref);
+
+  if (isVisible && !loadingWorkplans) {
+    setTimeout(() => loadMoreWorkplans(), 250);
+  }
 
   return (
-    <ETPageContainer
-      sx={{
-        paddingBottom: "0rem !important",
-      }}
-      container
-      spacing={2}
-    >
-      <Grid item xs={12} container justifyContent={"flex-end"}>
-        <AssigneeToggle />
-      </Grid>
-      <Grid item xs={12}>
-        <Filters />
-      </Grid>
-      <Grid item xs={12}>
-        <CardList />
-      </Grid>
-    </ETPageContainer>
+    <>
+      <ETPageContainer
+        sx={{
+          paddingBottom: "0rem !important",
+        }}
+        container
+        spacing={2}
+      >
+        <Grid item xs={12} container justifyContent={"flex-end"}>
+          <AssigneeToggle />
+        </Grid>
+        <Grid item xs={12}>
+          <Filters />
+        </Grid>
+        <Grid item xs={12}>
+          <CardList />
+        </Grid>
+        <Grid item ref={ref} />
+      </ETPageContainer>
+    </>
   );
 };
 
