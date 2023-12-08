a1f62f3610e13a04d1307b42593e3819f866db5d
Tom Chapman <tchapman000@gmail.com>
2023-12-08 08:23:28 -0800

    debug

--- epictrack-web/src/services/workplanService/index.tsx
+++ epictrack-web/src/services/workplanService/index.tsx
@@ -4,7 +4,6 @@ import { WorkPlan } from "../../models/workplan";
 
 class WorkPlanService {
   async getAll(page: number, size: number) {
-    console.log("PAGE", page);
     return await http.GetRequest<WorkPlan[]>(Endpoints.Workplan.GET_ALL, {
       page: page,
       size: size,
