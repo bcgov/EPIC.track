b78827037a97f617ceae9620b8bc1530ffc2c877
Jad <jad.saad@aot-technologies.com>
2023-12-13 14:57:39 -0800

    Enhancements for filterselect and name filter

--- epictrack-web/src/utils/MatchingTextHighlight.tsx
+++ epictrack-web/src/utils/MatchingTextHighlight.tsx
@@ -1,10 +1,13 @@
+import { Palette } from "../styles/theme";
+
+
 export const highlightText = (text: string, query: string) => {
   const index = text.toLowerCase().indexOf(query.toLowerCase());
   if (index !== -1) {
     return (
       <>
         {text.substring(0, index)}
-        <span style={{ backgroundColor: "rgb(255, 203, 127)" }}>
+        <span style={{ backgroundColor: Palette.secondary.main }}>
           {text.substring(index, index + query.length)}
         </span>
         {text.substring(index + query.length)}
