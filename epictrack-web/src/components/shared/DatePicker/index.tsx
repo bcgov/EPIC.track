8a085cced8cb8673be6786897e06fc901aa225c4
Jad <jad.saad@aot-technologies.com>
2023-12-19 14:10:47 -0800

    Add a standalone reusable DatePicker component

---
+++ epictrack-web/src/components/shared/DatePicker/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  LocalizationProvider,
+  DatePicker,
+  DatePickerSlotsComponentsProps,
+} from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DATE_FORMAT } from "../../../constants/application-constant";
+import { DatePickerProps } from "@mui/lab";
+
+type TrackDatePickerProps = DatePickerProps<any> & {
+  placeholder?: string;
+  slotProps?: DatePickerSlotsComponentsProps<unknown>;
+};
+
+const TrackDatePicker: React.FC<TrackDatePickerProps> = ({
+  placeholder,
+  slotProps,
+  ...rest
+}) => {
+  const { textField: textFieldProps, ...restSlotProps } = slotProps || {};
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DatePicker
+        views={["year", "month", "day"]}
+        format={DATE_FORMAT}
+        slotProps={{
+          textField: {
+            fullWidth: true,
+            placeholder: placeholder ?? DATE_FORMAT,
+            ...(textFieldProps ?? {}),
+          },
+          ...restSlotProps,
+        }}
+        sx={{ display: "block" }}
+        {...rest}
+      />
+    </LocalizationProvider>
+  );
+};
+
+export default TrackDatePicker;
