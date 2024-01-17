80408ef794aa2909295e5501ec6b51f17c1ad7ce
Salabh A N <salabh.n@aot-technologies.com>
2024-01-17 12:05:26 +0530

    Resource Forecast - Work name replacing Project name

--- epictrack-api/src/api/reports/resource_forecast_report.py
+++ epictrack-api/src/api/reports/resource_forecast_report.py
@@ -39,7 +39,7 @@ class EAResourceForeCastReport(ReportFactory):
     def __init__(self, filters):
         """Initialize the ReportFactory"""
         data_keys = [
-            "project_name",
+            "work_title",
             "capital_investment",
             "ea_type",
             "project_phase",
@@ -52,9 +52,6 @@ class EAResourceForeCastReport(ReportFactory):
             "work_lead",
             "env_region",
             "nrs_region",
-            # "staff_first_name",
-            # "staff_last_name",
-            # "role_id",
             "work_id",
             "ea_type_label",
             "sector(sub)",
@@ -87,7 +84,7 @@ class EAResourceForeCastReport(ReportFactory):
         self.month_labels = []
         self.report_cells = {
             "[PROJECT BACKGROUND]": [
-                {"data_key": "project_name", "label": "PROJECT NAME", "width": 0.050},
+                {"data_key": "work_title", "label": "WORK TITLE", "width": 0.050},
                 {
                     "data_key": "capital_investment",
                     "label": "EST. CAP. INVESTMENT",
@@ -257,7 +254,7 @@ class EAResourceForeCastReport(ReportFactory):
                 Work.id.in_(valid_work_ids),
             )
             .add_columns(
-                Project.name.label("project_name"),
+                Work.title.label("work_title"),
                 Project.capital_investment.label("capital_investment"),
                 WorkType.name.label("ea_type"),
                 WorkType.report_title.label("ea_type_label"),
@@ -481,7 +478,7 @@ class EAResourceForeCastReport(ReportFactory):
         data = self._format_data(data)
         if not data:
             return {}, None
-        data = sorted(data, key=lambda k: (k["ea_type_sort_order"], k["project_name"]))
+        data = sorted(data, key=lambda k: (k["ea_type_sort_order"], k["work_title"]))
         if return_type == "json" and data:
             return data, None
         formatted_data = defaultdict(list)
