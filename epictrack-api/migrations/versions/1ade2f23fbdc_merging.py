7cffeaff315cbebead2dcd0a9eb04ee04de8bce0
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2024-01-17 16:05:51 -0800

    Default special history insertion

---
+++ epictrack-api/migrations/versions/1ade2f23fbdc_merging.py
@@ -0,0 +1,24 @@
+"""merging
+
+Revision ID: 1ade2f23fbdc
+Revises: 52b37bc04925, f5d255d5de3b
+Create Date: 2024-01-17 16:05:00.273519
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1ade2f23fbdc'
+down_revision = ('52b37bc04925', 'f5d255d5de3b')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
