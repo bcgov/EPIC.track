f340e61c25d3345ad26057fd4d9ef39e553c804a
David Nunez <david.nunez@aot-technologies.com>
2024-01-31 08:27:12 -0500

    create test for date pickers

---
+++ epictrack-web/src/components/shared/__test__/TrackDatePicker.cy.tsx
@@ -0,0 +1,19 @@
+import TrackDatePicker from "../DatePicker/index";
+import React from "react";
+
+describe("TrackDatePicker", () => {
+  it("renders correctly and opens when clicked", () => {
+    cy.mount(<TrackDatePicker />);
+    // Check if the date picker is rendered
+    cy.get("button").should("be.visible");
+
+    // Click the date picker to open it
+    cy.get("button").click();
+
+    // Find the 'MuiDayCalendar-monthContainer' div and click the first button inside it
+    cy.get(".MuiDayCalendar-monthContainer").find("button").first().click();
+
+    // Check if the input field has any text
+    cy.get("input").should("not.have.value", "");
+  });
+});
