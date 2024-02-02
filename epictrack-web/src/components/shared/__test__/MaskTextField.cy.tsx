c43cadc67998057358d28fb41f8ebd812d192ccc
David Nunez <david.nunez@aot-technologies.com>
2024-02-02 01:25:31 -0500

    add mask testfield unit tests

---
+++ epictrack-web/src/components/shared/__test__/MaskTextField.cy.tsx
@@ -0,0 +1,31 @@
+import React, { ChangeEvent } from "react";
+import { useForm, FormProvider } from "react-hook-form";
+import { ControlledMaskTextField } from "../maskTextField";
+
+interface TestComponentProps {
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const TestComponent: React.FC<TestComponentProps> = ({ onChange }) => {
+  const methods = useForm();
+  return (
+    <FormProvider {...methods}>
+      <ControlledMaskTextField name="test" mask="####" onChange={onChange} />
+    </FormProvider>
+  );
+};
+describe("ControlledMaskTextField", () => {
+  it("renders correctly", () => {
+    cy.mount(<TestComponent />);
+    cy.get("input").should("exist");
+  });
+
+  it("handles input correctly", () => {
+    const handleChange = cy.stub();
+    cy.mount(<TestComponent onChange={handleChange} />);
+    cy.get("input").type("12341234");
+    cy.wrap(handleChange).should("have.been.calledWithMatch", {
+      target: { name: "test", value: "1234-1234" },
+    });
+  });
+});
