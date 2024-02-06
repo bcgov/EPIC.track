37e346b46e3275e6a05953108c7301865e272e90
David Nunez <david.nunez@aot-technologies.com>
2024-02-06 00:50:01 -0500

    add controlled tests

---
+++ epictrack-web/src/components/shared/__test__/ControlledSelectV2.cy.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useForm, FormProvider } from "react-hook-form";
+import ControlledSelectV2 from "../controlledInputComponents/ControlledSelectV2";
+
+interface TestComponentProps {
+  onChange?: (value: any) => void;
+}
+
+const options = [
+  { label: "Option 1", value: "option1" },
+  { label: "Option 2", value: "option2" },
+];
+
+const TestComponent: React.FC<TestComponentProps> = ({ onChange }) => {
+  const methods = useForm();
+  return (
+    <FormProvider {...methods}>
+      <ControlledSelectV2
+        name="test"
+        options={options}
+        getOptionLabel={(option: { label: any }) => option.label}
+        getOptionValue={(option: { value: any }) => option.value}
+        onHandleChange={onChange}
+      />
+    </FormProvider>
+  );
+};
+
+describe("ControlledSelectV2", () => {
+  it("renders correctly", () => {
+    cy.mount(<TestComponent />);
+    cy.get("#react-select-3-input").should("exist");
+  });
+
+  it("handles selection correctly", () => {
+    const handleChange = cy.stub();
+    cy.mount(<TestComponent onChange={handleChange} />);
+    cy.get("#react-select-3-input").click().type("Option 1{enter}");
+    cy.wrap(handleChange).should("have.been.calledWithMatch", {
+      target: { name: "test", value: "option1" },
+    });
+  });
+});
