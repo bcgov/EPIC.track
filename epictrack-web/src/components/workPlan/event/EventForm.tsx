97b039c51c3381a5b001cedb87c3fb257e64c0e1
dinesh <dinesh.pb@aot-technologies.com>
2024-03-08 22:12:41 -0800

    actual date max value for extension event

--- epictrack-web/src/components/workPlan/event/EventForm.tsx
+++ epictrack-web/src/components/workPlan/event/EventForm.tsx
@@ -229,20 +229,19 @@ const EventForm = ({
         : dayjs(ctx.selectedWorkPhase?.work_phase.start_date),
     [ctx.selectedWorkPhase, isStartEvent, isStartPhase]
   );
-
   const actualDateMax = useMemo(() => {
-    if (ctx.selectedWorkPhase?.work_phase.legislated) {
+    if (
+      ctx.selectedWorkPhase?.work_phase.legislated &&
+      selectedConfiguration?.event_category_id !== EventCategory.EXTENSION
+    ) {
       const diff = dayjs(ctx.selectedWorkPhase.work_phase.end_date).diff(
         ctx.selectedWorkPhase.work_phase.start_date,
         "day"
       );
-      return dayjs(ctx.selectedWorkPhase.work_phase.start_date).add(
-        diff,
-        "day"
-      );
+      return dayjs(ctx.selectedWorkPhase.work_phase.end_date);
     }
     return dayjs(new Date());
-  }, [ctx.selectedWorkPhase]);
+  }, [ctx.selectedWorkPhase, selectedConfiguration]);
 
   const methods = useForm({
     resolver: yupResolver(schema),
