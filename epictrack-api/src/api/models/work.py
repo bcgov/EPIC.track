295f00004c2b99d363c3b11408b668fd8663f80a
Jad <jad.saad@aot-technologies.com>
2024-03-18 15:29:10 -0700

    add sort by title as secondary sort

--- epictrack-api/src/api/models/work.py
+++ epictrack-api/src/api/models/work.py
@@ -125,7 +125,7 @@ class Work(BaseModelVersioned):
 
         query = cls.filter_by_search_criteria(query, search_filters)
 
-        query = query.order_by(Work.start_date.desc())
+        query = query.order_by(Work.start_date.desc(), Work.title)
 
         no_pagination_options = not pagination_options or not pagination_options.page or not pagination_options.size
         if no_pagination_options:
