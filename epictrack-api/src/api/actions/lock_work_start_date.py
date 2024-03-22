3134d5b1e1c56767aa80f966ad269bef5c0c517b
Dinesh <97143739+dinesh-aot@users.noreply.github.com>
2024-03-22 11:45:10 -0700

    template fixes and natsort block removed (#2026)

--- epictrack-api/src/api/actions/lock_work_start_date.py
+++ epictrack-api/src/api/actions/lock_work_start_date.py
@@ -1,5 +1,6 @@
 """Disable work start date action handler"""
 
+from datetime import timedelta
 from api.actions.base import ActionFactory
 from api.models.work import Work
 
@@ -11,5 +12,6 @@ class LockWorkStartDate(ActionFactory):  # pylint: disable=too-few-public-method
         """Set the work start date and mark start date as locked for changes"""
         work = Work.find_by_id(source_event.work_id)
         work.start_date_locked = params.get("start_date_locked")
-        work.start_date = source_event.actual_date
+        start_at = params.get("start_at") if params.get("start_at") else 0
+        work.start_date = source_event.actual_date + timedelta(days=start_at)
         work.update(work.as_dict(recursive=False), commit=False)
