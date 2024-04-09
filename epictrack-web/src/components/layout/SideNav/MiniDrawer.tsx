a7f4597d679bd5d8c5d4ff007b65b9a1fbbd19a8
jadmsaadaot <91914654+jadmsaadaot@users.noreply.github.com>
2024-04-09 05:02:22 -0700

    Create a minimized nav bar (#2082)
    * Add a collapsible sidenav
    
    * handle when screen is small

---
+++ epictrack-web/src/components/layout/SideNav/MiniDrawer.tsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+import { styled, Theme, CSSObject } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import { DrawerBox } from "./SideNav";
+import { useAppSelector } from "hooks";
+import { drawerExpandedWidth } from "styles/uiStateSlice";
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerExpandedWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerExpandedWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+export default function MiniDrawer() {
+  const { isDrawerExpanded: open } = useAppSelector((state) => state.uiState);
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader />
+        <Divider />
+        <DrawerBox />
+      </Drawer>
+    </Box>
+  );
+}
