740810536fadaae0294f65f72cc58952235cd06e
David Nunez <david.nunez@aot-technologies.com>
2024-05-03 11:45:27 -0400

    setup intercept status codes

--- epictrack-web/src/components/proponent/__test__/ProponentForm.cy.tsx
+++ epictrack-web/src/components/proponent/__test__/ProponentForm.cy.tsx
@@ -93,18 +93,16 @@ function createMockContext() {
 
 function setupIntercepts(endpoints: any[]) {
   endpoints.forEach(({ method, url, response, name }) => {
-    if (method === "OPTIONS") {
-      // Add CORS headers specifically for OPTIONS requests
-      response = {
-        ...response,
-        headers: {
-          "Access-Control-Allow-Origin": "*", // Allow all domains
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // Specify allowed methods
-          "Access-Control-Allow-Headers": "Content-Type, Authorization", // Specify allowed headers
-        },
-        statusCode: 200, // Ensure the response is marked as successful
-      };
-    }
+    // Add CORS headers specifically for OPTIONS requests
+    response = {
+      ...response,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Allow all domains
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // Specify allowed methods
+        "Access-Control-Allow-Headers": "Content-Type, Authorization", // Specify allowed headers
+      },
+      statusCode: 200, // Ensure the response is marked as successful
+    };
 
     cy.intercept(method, url, response).as(name);
   });
--- epictrack-web/src/components/proponent/__test__/ProponentList.cy.tsx
+++ epictrack-web/src/components/proponent/__test__/ProponentList.cy.tsx
@@ -31,6 +31,15 @@ const proponents = [proponent1, proponent2];
 
 function setupIntercepts(endpoints: any[]) {
   endpoints.forEach(({ method, url, response, name }) => {
+    response = {
+      ...response,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Allow all domains
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // Specify allowed methods
+        "Access-Control-Allow-Headers": "Content-Type, Authorization", // Specify allowed headers
+      },
+      statusCode: 200, // Ensure the response is marked as successful
+    };
     cy.intercept(method, url, response).as(name);
   });
 }
