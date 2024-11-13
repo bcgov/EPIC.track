1ca855eb6b2638b5ce1a7952bbde8985221507e0
Marilynn Stone <MarilynnStone@proton.me>
2024-11-13 22:02:53 +0000

    Remove unnecessary changes.

--- epictrack-web/src/components/work/WorkForm/WorkLeadSpecialField.tsx
+++ epictrack-web/src/components/work/WorkForm/WorkLeadSpecialField.tsx
@@ -63,7 +63,7 @@ export const WorkLeadSpecialField = ({
           <SpecialFieldGrid
             entity={SpecialFieldEntityEnum.WORK}
             entity_id={id}
-            fieldName={SPECIAL_FIELDS.WORK.TEAM_LEAD}
+            fieldName={SPECIAL_FIELDS.WORK.WORK_LEAD}
             fieldLabel={"Work Lead"}
             fieldType={"select"}
             title={"Work Lead History"}
