2ff6a431e1c742647283ae9122b806f6c44f398f
Corey Koelewyn <34955494+Ckoelewyn@users.noreply.github.com>
2025-03-06 14:29:15 -0800

    TRACK-326-2: Enable Save button for team members on work dialog (#2551)

--- epictrack-web/src/components/work/Dialog/index.tsx
+++ epictrack-web/src/components/work/Dialog/index.tsx
@@ -9,26 +9,29 @@ import { hasPermission } from "components/shared/restricted";
 import { ROLES } from "constants/application-constant";
 
 type WorkDialogProps = {
+  isActiveTeamMember: boolean;
   open: boolean;
   setOpen: (open: boolean) => void;
   workId?: number;
-  saveWorkCallback?: () => void;
   closeCallback?: () => void;
+  saveWorkCallback?: () => void;
 };
 export const WorkDialog = ({
-  workId,
+  isActiveTeamMember,
   open,
   setOpen,
-  saveWorkCallback = () => {
+  workId,
+  closeCallback = () => {
     return;
   },
-  closeCallback = () => {
+  saveWorkCallback = () => {
     return;
   },
 }: WorkDialogProps) => {
   const [work, setWork] = useState<Work | null>(null);
   const { roles } = useAppSelector((state) => state.user.userDetail);
-  const canEdit = hasPermission({ roles, allowed: [ROLES.EDIT] });
+  const canEdit =
+    hasPermission({ roles, allowed: [ROLES.EDIT] }) || isActiveTeamMember;
   const [disableSave, setDisableSave] = useState(!canEdit);
 
   const fetchWork = async () => {
