2c65f6d3e6a1e787ef635ac280631f1753d0eea4
Shaelyn Tolkamp <shaelyn.tolk@gmail.com>
2025-06-04 16:38:18 -0700

    remove log

--- epictrack-web/src/components/shared/MasterTrackTable/index.tsx
+++ epictrack-web/src/components/shared/MasterTrackTable/index.tsx
@@ -85,8 +85,6 @@ const MasterTrackTable = <TData extends MRT_RowData>({
 }: MaterialReactTableProps<TData>) => {
   const { initialState, state, icons } = rest;
 
-  console.log("MasterTrackTable render");
-
   const table = useMaterialReactTable({
     columns: columns,
     data: data,
