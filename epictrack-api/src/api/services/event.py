bc538ddf2e610479faead30da72076fe43773775
Corey Koelewyn <corey.koelewyn@outlook.com>
2025-07-22 10:36:12 -0700

    [TRACK-543] Setting START event actual unlocks END event anticipated

--- epictrack-api/src/api/services/event.py
+++ epictrack-api/src/api/services/event.py
@@ -293,7 +293,6 @@ class EventService:
     ) -> None:
         # pylint: disable=too-many-arguments
         """Process the event date logic"""
-        cls._end_event_anticipated_change_rule(event, event_old, current_work_phase)
         all_work_phases = WorkPhase.find_by_params(
             {
                 "work_id": current_work_phase.work_id,
@@ -305,7 +304,7 @@ class EventService:
             all_work_phases, current_work_phase
         )
         cls._validate_dates(event, current_work_phase, all_work_phases)
-        cls._previous_event_acutal_date_rule(
+        cls._previous_event_actual_date_rule(
             all_work_events, all_work_phases, current_work_phase_index, event, event_old
         )
         cls._handle_work_start_date_for_start_event_start_phase(
@@ -373,6 +372,7 @@ class EventService:
         current_future_work_phases = all_work_phases[current_work_phase_index:]
         # if the phase is legislated, only start event, extension or suspension can push
         # all the subsequent events in all the subsequent phases
+        # CK TRACK-543 : We want end event in legislated phase to push subsequent events also.
         if current_work_phase.legislated:
             phase_events = cls._find_work_phase_events(
                 all_work_events, current_work_phase.id
@@ -380,6 +380,8 @@ class EventService:
             if (
                 event.event_configuration.event_position.value
                 == EventPositionEnum.START.value
+                or event.event_configuration.event_position.value
+                == EventPositionEnum.END.value
                 or event.event_configuration.event_category_id
                 in [
                     EventCategoryEnum.EXTENSION.value,
@@ -861,22 +863,7 @@ class EventService:
         return phase_events
 
     @classmethod
-    def _end_event_anticipated_change_rule(
-        cls, event: Event, event_old: Event, current_work_phase: WorkPhase
-    ) -> None:
-        """Anticipated date of end event of legislated phase cannot be changed"""
-        if (
-            current_work_phase.legislated
-            and event.event_position == EventPositionEnum.END.value
-            and (event_old.anticipated_date.date() - event.anticipated_date.date()).days
-            != 0
-        ):
-            raise UnprocessableEntityError(
-                "Anticipated date of the phase end event should not be changed"
-            )
-
-    @classmethod
-    def _previous_event_acutal_date_rule(  # pylint: disable=too-many-arguments
+    def _previous_event_actual_date_rule(  # pylint: disable=too-many-arguments
         cls,
         all_work_events: List[Event],
         all_work_phases: List[WorkPhase],
