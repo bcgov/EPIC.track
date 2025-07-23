24602603f43d20e93369e3823bb8762af07889d9
Corey Koelewyn <34955494+Ckoelewyn@users.noreply.github.com>
2025-07-23 15:02:47 -0700

    [TRACK-582] update default filters on work list (#2634)

--- epictrack-web/src/components/work/WorkList.tsx
+++ epictrack-web/src/components/work/WorkList.tsx
@@ -38,8 +38,8 @@ const WorkList = () => {
     All_WORKS_FILTERS_CACHE_KEY,
     [
       {
-        id: "is_active",
-        value: [true],
+        id: "work_state",
+        value: ["IN_PROGRESS", "COMPLETED"],
       },
     ]
   );
