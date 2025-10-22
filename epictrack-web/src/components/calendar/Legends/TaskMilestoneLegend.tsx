ba9e03cc01ef38a1bcc59f54ab939a90a67f5b06
Tom Chapman <tchapman000@gmail.com>
2025-10-22 09:55:32 -0700

    TRACK-602 changes to calendar legend toggles style

--- epictrack-web/src/components/calendar/Legends/TaskMilestoneLegend.tsx
+++ epictrack-web/src/components/calendar/Legends/TaskMilestoneLegend.tsx
@@ -1,7 +1,6 @@
 import { Box, ToggleButton } from "@mui/material";
 import { LEGEND_COLOURS } from "../constants";
 import { ETCaption1 } from "components/shared";
-import CheckIcon from "@mui/icons-material/Check";
 import { useEventCalendarContext } from "../EventCalendarContext";
 
 const TaskMilestoneLegend = () => {
@@ -14,70 +13,60 @@ const TaskMilestoneLegend = () => {
 
   return (
     <Box display="flex" alignItems="center" gap={2} m={2}>
-      <Box display="flex" alignItems="center" gap={1}>
+      <Box
+        display="flex"
+        alignItems="center"
+        gap={1}
+        sx={{
+          cursor: "pointer",
+          p: 1,
+          backgroundColor: milestoneSelected ? "#F9E9C4" : "transparent", // highlight only when selected
+          borderRadius: "3px",
+        }}
+        onClick={() => setMilestoneSelected((prev) => !prev)}
+      >
         <ToggleButton
           value="milestone"
           selected={milestoneSelected}
-          onChange={() => setMilestoneSelected((prev) => !prev)}
           sx={{
-            bgcolor: LEGEND_COLOURS.backgroundColour.MILESTONE,
+            bgcolor: LEGEND_COLOURS.backgroundColour.MILESTONE + "!important",
             borderColor: LEGEND_COLOURS.border.MILESTONE,
             borderRadius: "2px",
             border: "1px solid",
             minWidth: "1rem",
             minHeight: "1rem",
             padding: 0,
-            "&:active, &:hover": {
-              backgroundColor: LEGEND_COLOURS.backgroundColour.MILESTONE,
-            },
-            "&.Mui-selected": {
-              backgroundColor: LEGEND_COLOURS.backgroundColour.MILESTONE,
-            },
-            "&.Mui-selected:hover": {
-              backgroundColor: LEGEND_COLOURS.backgroundColour.MILESTONE,
-            },
+            pointerEvents: "none",
           }}
-        >
-          <CheckIcon
-            sx={{
-              fontSize: "1rem",
-              visibility: milestoneSelected ? "visible" : "hidden",
-            }}
-          />
-        </ToggleButton>
+        />
         <ETCaption1 variant="body2">Milestones</ETCaption1>
       </Box>
-      <Box display="flex" alignItems="center" gap={1}>
+      <Box
+        display="flex"
+        alignItems="center"
+        gap={1}
+        sx={{
+          cursor: "pointer",
+          p: 1,
+          backgroundColor: taskSelected ? "#F9E9C4" : "transparent",
+          borderRadius: "3px",
+        }}
+        onClick={() => setTaskSelected((prev) => !prev)}
+      >
         <ToggleButton
           value="task"
           selected={taskSelected}
-          onChange={() => setTaskSelected((prev) => !prev)}
           sx={{
-            bgcolor: LEGEND_COLOURS.backgroundColour.TASK,
+            bgcolor: LEGEND_COLOURS.backgroundColour.TASK + "!important",
             borderColor: LEGEND_COLOURS.border.TASK,
             borderRadius: "2px",
             border: "1px solid",
             minWidth: "1rem",
             minHeight: "1rem",
             padding: 0,
-            "&:active, &:hover": {
-              backgroundColor: LEGEND_COLOURS.backgroundColour.TASK,
-            },
-            "&.Mui-selected": {
-              backgroundColor: LEGEND_COLOURS.backgroundColour.TASK,
-            },
-            "&.Mui-selected:hover": {
-              backgroundColor: LEGEND_COLOURS.backgroundColour.TASK,
-            },
+            pointerEvents: "none",
           }}
-        >
-          <CheckIcon
-            sx={{
-              fontSize: "1rem",
-              visibility: taskSelected ? "visible" : "hidden",
-            }}
-          />
-        </ToggleButton>
+        />
         <ETCaption1 variant="body2">Tasks</ETCaption1>
       </Box>
     </Box>
