5a96d8b1f6a6e02e2e8ab374d4eb4d10897f64f4
Corey Koelewyn <corey.koelewyn@outlook.com>
2025-11-07 15:12:39 -0800

    [Track-666] Change event date cascading errors gracefully

--- epictrack-api/src/api/exceptions/__init__.py
+++ epictrack-api/src/api/exceptions/__init__.py
@@ -81,3 +81,12 @@ class UnprocessableEntityError(UnprocessableEntity):
         super().__init__(*args, **kwargs)
         self.description = message
         self.response = Response(message, status=422)
+
+class UnprocessableEventError(UnprocessableEntity):
+    """Exception raised when resource is not processable"""
+
+    def __init__(self, message, *args, **kwargs):
+        """Return a valid UnprocessableEntityError."""
+        super().__init__(*args, **kwargs)
+        self.description =  { "message": message , "code": "UnprocessableEventError" }
+        self.response = Response(self.description, status=422)
