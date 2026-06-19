cff52d44a97e5c2f3fdfbe6736d4cb521d9dc964
Tom Chapman <tchapman000@gmail.com>
2026-06-19 14:37:52 -0700

    fix tests

--- epictrack-api/tests/utilities/factory_scenarios.py
+++ epictrack-api/tests/utilities/factory_scenarios.py
@@ -220,7 +220,24 @@ class TestJwtClaims(dict, Enum):
         'email': 'example@example.com',
         'realm_access': {
             'roles': [
+                'extended_edit',
                 'manage_users'
+            ]
+        }
+    }
+
+    view_only_role = {
+        'iss': CONFIG.JWT_OIDC_TEST_ISSUER,
+        'sub': 'f7a4a1d3-73a8-4cbc-a40f-bb1145302064',
+        'idp_userid': 'f7a4a1d3-73a8-4cbc-a40f-bb1145302064',
+        'preferred_username': f'{fake.user_name()}@idir',
+        'given_name': fake.first_name(),
+        'family_name': fake.last_name(),
+        'tenant_id': 1,
+        'email': 'example@example.com',
+        'realm_access': {
+            'roles': [
+                'view'
             ]
         }
     }
